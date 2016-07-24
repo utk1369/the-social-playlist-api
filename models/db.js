5b996ee439d257fc86f99561d96f54eb4670ba3e
utk1369 <utkarsh1369.11@bitmesra.ac.in>
2016-07-24 15:13:50 +0530

    Changed MongoDB deployment

--- models/db.js
+++ models/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/the-social-playlist');
+mongoose.connect('mongodb://utk:tspdb@ds027215.mlab.com:27215/the_social_playlist_db');
